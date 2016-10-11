ccaf77f3a25eb3f0f36efb9e8afdd9cbc755cc9e
tridays <tridays@qq.com>
2016-10-11 21:28:43 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+JAR="librarian-1.0-SNAPSHOT.jar"
+MAIN_CLASS="xp.librarian.Application"
+function get_pids() {
+        PIDS=`ps x | grep ${JAR} | grep -v grep | grep java | awk '{print $1}'`
+}
+function stop() {
+        get_pids
+        for PID in ${PIDS};do
+                kill ${PID}
+                echo "killing: ${PID}"
+        done
+        while [ $PIDS ]
+        do
+                sleep 1
+                echo "waiting exited: ${PIDS}"
+                get_pids
+        done
+}
+function start() {
+        nohup java -jar ${JAR} --spring.profiles.active=production 1>app.log 2>app.err &
+        sleep 1
+}
+stop
+start
+
