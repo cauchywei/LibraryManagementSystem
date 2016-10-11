00ed62887d9780e7ec76088754b1ba1a90bec43b
tridays <tridays@qq.com>
2016-10-12 04:24:06 +0800

    indent

--- run.sh
+++ run.sh
@@ -4,32 +4,32 @@ JAR="librarian-1.0-SNAPSHOT.jar"
 MAIN_CLASS="xp.librarian.Application"
 
 function get_pids() {
-        PIDS=`ps x | grep ${JAR} | grep -v grep | grep java | awk '{print $1}'`
+    PIDS=`ps x | grep ${JAR} | grep -v grep | grep java | awk '{print $1}'`
 }
 
 function dump() {
-        get_pids
-        echo "running: ${PIDS}"
+    get_pids
+    echo "running: ${PIDS}"
 }
 function stop() {
-        dump
+    dump
 
-        for PID in ${PIDS};do
-                kill ${PID}
-                echo "killing: ${PID}"
-        done
+    for PID in ${PIDS};do
+        kill ${PID}
+        echo "killing: ${PID}"
+    done
 
-        while [ $PIDS ];do
-                sleep 1
-                echo "waiting exited: ${PIDS}"
-                get_pids
-        done
+    while [ $PIDS ];do
+        sleep 1
+        echo "waiting exited: ${PIDS}"
+        get_pids
+    done
 }
 
 function start() {
-        nohup java -jar ${JAR} --spring.profiles.active=production 1>app.log 2>app.err &
-        sleep 1
-        dump
+    nohup java -jar ${JAR} --spring.profiles.active=production 1>app.log 2>app.err &
+    sleep 1
+    dump
 }
 
 stop
